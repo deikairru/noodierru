5eb6142da9776169465525bdf47195725b7aec67
deikairru <deikairru@gmail.com>
2024-08-01 16:10:07 +0700

    component: add responsive on header and hero section

--- app/header.tsx
+++ app/header.tsx
@@ -10,22 +10,22 @@ export function Header() {
         <GiNoodles size={30} />
       </div>
 
-      <div className="nav font-medium flex flex-row justify-between items-center underline-offset-[12px] decoration-4 decoration-orange-400 [&>a:hover]:underline [&>a]:px-4">
+      <div className="hidden lg:flex nav font-medium flex-row justify-between items-center underline-offset-[12px] decoration-4 decoration-orange-400 [&>a:hover]:underline [&>a]:px-4">
         <a href="#Home">Home</a>
         <a href="#Services">Services</a>
         <a href="#Menu">Menu</a>
         <a href="#Review">Review</a>
         <a href="#Contact">Contact</a>
       </div>
-      <div className="cta flex flex-row justify-center items-center space-x-6">
+      <div className="cta hidden md:flex flex-row justify-center items-center space-x-6">
         <div className="search hover:text-orange-400 hover:cursor-pointer">
           <FaSearch size={25} />
         </div>
-        <div className="cart relative [&>*:first-child:hover]:text-orange-400 [&>*:first-child:hover]:cursor-pointer">
+        <div className="hidden md:display cart relative [&>*:first-child:hover]:text-orange-400 [&>*:first-child:hover]:cursor-pointer">
           <FaShoppingCart size={25} />
           <span className="text-center rounded-full p-1.5 absolute -top-4 -right-4 text-[10px] bg-orange-400 leading-none">10</span>
         </div>
-        <div className="sign-up flex justify-center items-center px-4 py-1 space-x-2 rounded-full bg-orange-400 hover:text-white hover:cursor-pointer shadow-md drop-shadow-md">
+        <div className="hidden sign-up md:flex justify-center items-center px-4 py-1 space-x-2 rounded-full bg-orange-400 hover:text-white hover:cursor-pointer shadow-md drop-shadow-md">
           <FiLogIn size={20} />
           <p className="font-semibold">Login</p>
         </div>
--- app/hero.tsx
+++ app/hero.tsx
@@ -5,43 +5,55 @@ import FoodPlate3 from "../public/foodplate3.png";
 
 export function Hero() {
   return (
-    <div id='Home' className="hero w-4/5 grid grid-cols-2 space-x-10">
-      <div className="hero-desc relative flex flex-col h-screen justify-center gap-y-4">
-        <h1 className="text-5xl w-4/5 font-semibold">Be The Fastest in Delivery Your <span className="text-orange-400">Food</span></h1>
-        <p className="w-3/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, omnis soluta? Fugiat a quam sed!</p>
-        <div className="bg-orange-400 font-semibold py-2 px-6 self-start rounded-full text-center hover:text-white hover:cursor-pointer mt-2 shadow-xl drop-shadow-xl">Get Started</div>
-        <div className="p-0 w-10 h-10 bg-orange-400 absolute bottom-48 right-4 rounded-full"></div>
+    <div id='Home' className="hero w-full py-32 sm:py-48 sm:h-[60%] md:h-[80%] lg:h-screen grid grid-cols-1 lg:grid-cols-2 gap-2">
+      <div className="size-60 lg:hidden blur-2xl bg-orange-200 absolute -top-32 -right-20 rounded-full" />
+
+      <div className="hero-desc relative max-w-[480px] flex flex-col justify-center gap-y-4">
+        {/* Background Shapes */}
+        <div className="size-60 lg:hidden blur-2xl bg-orange-200 absolute -bottom-40 -right-80 rounded-full" />
+        <div className="size-32 sm:hidden blur-2xl bg-orange-200 absolute -bottom-24 right-10 rounded-full" />
+        <div className="size-20 bg-orange-200 blur-2xl absolute -top-10 -left-16 rounded-full" />
+
+        {/* Hero */}
+        <h1 className="text-4xl mr-8 block text-pretty font-bold">Be the fastest in receive your <span className="text-orange-400">Food</span></h1>
+        <p className="max-w-[2/3] mr-auto text-pretty">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, omnis soluta? Fugiat a quam sed!</p>
+        <div className="bg-orange-400 font-semibold py-2 px-4 text-sm self-start rounded-full text-center hover:text-white hover:cursor-pointer mt-2 shadow-xl drop-shadow-xl">Get Started</div>
       </div>
-      <div className="hero-images relative flex flex-col justify-center items-center space-y-10">
-        <div className="p-10 w-50 h-50 bg-orange-400 absolute top-36 left-5 rounded-full"></div>
-        <div className="p-10 w-50 h-50 bg-orange-400 absolute center right-5 rounded-full"></div>
-        <div className="p-0 w-5 h-5 bg-orange-400 absolute center -translate-y-10 left-20 rounded-full"></div>
-        <div className="p-0 w-[300px] h-[300px] rounded-full bg-transparent absolute top-24 center translate-y-1 -translate-x-2 shadow-[rgba(0,0,15,0.15)_10px_10px_10px_2px]"></div>
-        <Image
-          src={FoodPlate2}
-          width={344}
-          height={344}
-          alt="Picture of food"
-          className="absolute center top-20"
-        />
 
-        <div className="p-0 w-[170px] h-[170px] rounded-full bg-transparent absolute bottom-44 left-4 shadow-[rgba(0,0,15,0.15)_10px_10px_10px_2px]"></div>
-        <Image
-          src={FoodPlate3}
-          width={200}
-          height={200}
-          alt="Picture of food"
-          className="absolute bottom-40 left-0"
-        />
+      <div className="hero-images relative hidden lg:flex flex-col justify-center items-center space-y-10">
+        <div className="size-[300px] blur-3xl bg-orange-200 absolute -top-80 -right-40 rounded-full"></div>
+        <div className="size-32 blur-3xl bg-orange-200 absolute -top-80 -left-20 rounded-full"></div>
+        <div className="size-60 blur-2xl bg-orange-200 absolute -bottom-20 -left-40 rounded-full"></div>
 
-        <Image
-          src={FoodPlate}
-          width={275}
-          height={275}
-          alt="Picture of food"
-          className="absolute bottom-10 right-20"
-        />
-        <div className="p-0 w-[230px] h-[230px] rounded-full bg-transparent absolute bottom-14 right-28 translate-x-2 shadow-[rgba(0,0,15,0.1)_10px_10px_10px_2px]"></div>
+        <div className="absolute center">
+          <div className="p-0 w-[300px] h-[300px] rounded-full bg-transparent absolute translate-y-5 translate-x-4 shadow-2xl drop-shadow-2xl" />
+          <Image
+            src={FoodPlate2}
+            width={344}
+            height={344}
+            alt="Picture of food"
+          />
+        </div>
+
+        <div className="hidden xl:block absolute -top-20 -left-20">
+          <div className="hidden xl:block p-0 w-[170px] h-[170px] rounded-full bg-transparent absolute top-4 left-4 shadow-2xl drop-shadow-2xl" />
+          <Image
+            src={FoodPlate3}
+            width={200}
+            height={200}
+            alt="Picture of food"
+          />
+        </div>
+
+        <div className="hidden xl:block absolute -bottom-40 -right-20">
+          <div className="p-0 w-[230px] h-[230px] rounded-full bg-transparent absolute translate-x-4 translate-y-4 shadow-2xl drop-shadow-2xl" />
+          <Image
+            src={FoodPlate}
+            width={275}
+            height={275}
+            alt="Picture of food"
+          />
+        </div>
       </div>
     </div >
   );
